5da94d3a5c7568c445ad69bd8421e79ddabf147f
Bonnie Williams <boxerpuplovr@gmail.com>
2018-09-06 03:53:21 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    #don't need to rewrite what is written in db file in Rails
+  #don't need to rewrite what is written in db file in Rails
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
